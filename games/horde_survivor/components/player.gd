c999735717618221d97b2c6a6b52cdead658fb37
Vinícius Hoyer <contact@vhoyer.dev>
2024-07-30 00:26:43 -0300

    ✨ Knight has walking animation

--- games/horde_survivor/components/player.gd
+++ games/horde_survivor/components/player.gd
@@ -5,5 +5,12 @@ var speed = 400  # speed in pixels/sec
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite2D.play("idle")
+	
 
 	move_and_slide()
