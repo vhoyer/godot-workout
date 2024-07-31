93f8b095f4f1ee8ac77dbc7300244af2b5eb3139
Vinícius Hoyer <contact@vhoyer.dev>
2024-07-31 10:16:04 -0300

    ⚗️ Convert player animation into animation tree

--- games/horde_survivor/components/player.gd
+++ games/horde_survivor/components/player.gd
@@ -1,30 +1,19 @@
 extends CharacterBody2D
 
 var speed = 400  # speed in pixels/sec
+var direction
+
+@onready var animation_tree = $AnimationTree
 
 func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
 
-var is_finished = true
-var animation_queue: Array[String] = []
-
-func _ready():
-	$AnimatedSprite2D.animation_finished.connect(finish)
-
-func finish(): is_finished = true
-
 func _process(delta):
-	var animation = "idle"
-	if velocity.length() > 0:
-		animation = "walk"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	
-	if Input.is_action_pressed("attack"):
-		animation = "attack-side-one"
-	
-	if (is_finished):
-		is_finished = false
-		$AnimatedSprite2D.play(animation)
-
+	print(direction.x)
+	animation_tree.set("parameters/Flip/blend_position", direction.x)
+	animation_tree.set("parameters/Animation/attack_1/blend_position", direction.y)
+	animation_tree.set("parameters/Animation/attack_2/blend_position", direction.y)
+	animation_tree.set("parameters/Animation/conditions/walking", velocity.length() > 0)
+	animation_tree.set("parameters/Animation/conditions/not_walking", velocity.length() == 0)
