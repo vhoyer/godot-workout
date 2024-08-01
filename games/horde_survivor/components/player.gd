c1c288f3242ee6af3e8aca5a054428c1c61881cc
Vinícius Hoyer <contact@vhoyer.dev>
2024-08-01 00:01:34 -0300

    ✨ Add combo animation to player

--- games/horde_survivor/components/player.gd
+++ games/horde_survivor/components/player.gd
@@ -2,48 +2,44 @@ extends CharacterBody2D
 
 var speed = 400  # speed in pixels/sec
 var direction
-var combo = 0
+var attack = 0
 
 @onready var animation_tree = $AnimationTree
-@onready var combo_timer = $ComboTimer
-@onready var animation_player = $AnimationPlayer
+var state_machine: AnimationNodeStateMachinePlayback
 
-func get_speed():
-	if is_attacking():
-		return speed / 4
-	return speed
+@onready var animation_player = $AnimationPlayer
+@onready var combo_timer = $ComboTimer
+
+func _ready():
+	state_machine = animation_tree.get("parameters/Animation/playback");
 
 func _physics_process(delta):
-	process_combo(delta)
-	
-	direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * get_speed()
+	process_combo()
+	process_movement()
 	move_and_slide()
 
-func process_combo(delta):
-	if (Input.is_action_just_pressed("attack")):
-		combo += 1
-	if combo > 0 and combo_timer.is_stopped():
-		combo_timer.start(1)
-	if combo > 2:
-		combo = 0
+func process_movement():
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	if (attack > 0): velocity = Vector2.ZERO
 
-func reset_combo():
-	combo = 0
+func process_combo():
+	if (!Input.is_action_just_pressed("attack")): return
+	var current = state_machine.get_current_node()
+	combo_timer.start(0.7)
+	if (attack == 0):
+		state_machine.travel("attack_1")
+	elif (attack == 1 && current == "attack_1"):
+		state_machine.travel("attack_2")
+	attack += 1
 
-func is_attacking():
-	return combo > 0 || animation_player.is_playing()
+func _on_combo_timer_timeout():
+	attack = 0
 
 func _process(delta):
 	if direction.x != 0:
 		animation_tree.set("parameters/Flip/blend_position", direction.x)
-	if direction.y != 0:
-		animation_tree.set("parameters/Animation/attack_1/blend_position", direction.y)
-		animation_tree.set("parameters/Animation/attack_2/blend_position", direction.y)
+	animation_tree.set("parameters/Animation/attack_1/blend_position", direction.y)
+	animation_tree.set("parameters/Animation/attack_2/blend_position", direction.y)
 	animation_tree.set("parameters/Animation/conditions/walking", velocity.length() > 0)
 	animation_tree.set("parameters/Animation/conditions/not_walking", velocity.length() == 0)
-	animation_tree.set("parameters/Animation/conditions/combo_1", combo >= 1)
-	animation_tree.set("parameters/Animation/conditions/combo_2", combo >= 2)
-
-func _on_combo_timer_timeout():
-	reset_combo()
