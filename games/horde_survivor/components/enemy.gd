175bdb3f94b9c1c5e4ab43cf767a7792bb8600ff
Vinícius Hoyer <contact@vhoyer.dev>
2024-08-13 09:15:56 -0300

    ✨ Enemies try attacking player

--- games/horde_survivor/components/enemy.gd
+++ games/horde_survivor/components/enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animation_tree = $AnimationTree
 var state_machine: AnimationNodeStateMachinePlayback
+var try_attacking = false;
 
 @export var HP = 2;
 @export var Speed = 250;
@@ -14,22 +15,25 @@ func _ready():
 
 func _physics_process(delta):
 	velocity = lerp(velocity, Vector2.ZERO, 0.1);
-	var current = state_machine.get_current_node()
 	
+	var current = state_machine.get_current_node()
 	var voluntary_states: Array[String] = ['idle', 'walk'];
+	
 	if (voluntary_states.has(current)):
 		process_movement(delta);
 	
 	move_and_slide();
 
 func process_movement(delta):
-	var direction = node_to_follow.position - self.position;
-	velocity += direction.normalized() * Speed * delta
-	pass
+	if (try_attacking):
+		state_machine.travel("attack");
+	else:
+		var direction = node_to_follow.position - self.position;
+		velocity += direction.normalized() * Speed * delta
 
 func _process(_delta):
 	var not_walking_velocity = 10;
-	if velocity.x > not_walking_velocity:
+	if velocity.x > 0:
 		animation_tree.set("parameters/Flip/blend_position", velocity.x)
 	if velocity.length() > not_walking_velocity:
 		animation_tree.set("parameters/Animation/attack/blend_position", velocity.y)
@@ -42,10 +46,17 @@ func drop_loot(): # called by animation
 func take_damage(attack: AttackHitbox):
 	HP -= 1;
 	if (HP <= 0):
-		state_machine.travel("despawn");
+		state_machine.travel("death");
 	else:
 		state_machine.travel("damage");
 		velocity += attack.knockback_velocity(self);
 
 func _on_hitbox_area_entered(attack: AttackHitbox):
 	take_damage(attack)
+
+func _on_melee_range_body_entered(body):
+	try_attacking = true;
+
+
+func _on_melee_range_body_exited(body):
+	try_attacking = false;
